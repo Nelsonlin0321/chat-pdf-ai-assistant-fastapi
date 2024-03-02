8cce0367b7a1f7cd88d8051836b13ad1a79969f5
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-03-02 17:21:03 +0800

    update search endpoint

--- app/routers/v1/endpoints.py
+++ app/routers/v1/endpoints.py
@@ -6,6 +6,7 @@ from app import utils
 import boto3
 import os
 from app.mongodb_engine import MongoDB
+import pandas as pd
 
 gcp_secret = os.environ.get('GCP_SECRET')
 
@@ -75,7 +76,78 @@ def vector_search(query: str, file_key: str, limit: int = 5):
 
 
 @router.post("/keyword_search")
-def vector_search(query: str, file_key: str, limit: int = 5):
+def keyword_search(query: str, file_key: str, limit: int = 5):
     results = mongo_db_engine.keyword_search(
         query=query, file_key=file_key, limit=limit)
+    return results
+
+
+@router.post("/hybrid_search")
+def hybrid_search(query: str, file_key: str, limit: int = 5):
+    keyword_search_results = keyword_search(
+        query=query, file_key=file_key, limit=limit)
+    vector_search_results = vector_search(
+        query=query, file_key=file_key, limit=limit)
+    results = combine_vector_keyword_search(vector_search_results=vector_search_results,
+                                            keyword_search_results=keyword_search_results, limit=limit)
+    return results
+
+
+def normalized_score(df):
+    max_score = max(df['score'])
+    min_score = min(df['score'])
+    if (max_score-min_score) == 0:
+        df['normalized_score'] = 1
+    else:
+        df['normalized_score'] = df['score'].apply(
+            lambda x: (x-min_score)/(max_score-min_score))
+    return df
+
+
+def combine_vector_keyword_search(vector_search_results, keyword_search_results, limit: int = 5):
+
+    chunk_id = "chunk_id"
+
+    if len(vector_search_results) == 0 and len(keyword_search_results) == 0:
+        return []
+
+    if len(vector_search_results) == 0:
+        return vector_search_results
+
+    if len(keyword_search_results) == 0:
+        return keyword_search_results
+
+    df_keyword_search = pd.DataFrame(keyword_search_results)
+    df_vector_search = pd.DataFrame(vector_search_results)
+
+    both_hit_indices = set(df_vector_search[[chunk_id]].merge(
+        df_keyword_search[[chunk_id]])[chunk_id])
+
+    rest_size = limit - len(both_hit_indices)
+
+    df_vector_search = normalized_score(df_vector_search)
+    df_vector_search['search_type'] = "vector_search"
+    df_keyword_search = normalized_score(df_keyword_search)
+    df_keyword_search['search_type'] = "keyword_search"
+
+    df_hybrid_search = pd.concat(
+        [df_keyword_search, df_vector_search])
+
+    df_hybrid_search_1 = df_keyword_search[df_keyword_search[chunk_id].isin(
+        both_hit_indices)].copy()
+
+    df_hybrid_search_1['both_hit'] = True
+
+    df_hybrid_search_2 = df_hybrid_search[~df_hybrid_search[chunk_id].isin(
+        both_hit_indices)].copy()
+
+    df_hybrid_search_2 = df_hybrid_search_2.sort_values(
+        by='normalized_score', ascending=False).head(rest_size)
+
+    df_hybrid_search_2['both_hit'] = False
+
+    df_hybrid_search = pd.concat([df_hybrid_search_1, df_hybrid_search_2])
+
+    results = df_hybrid_search.to_dict(orient='records')
+
     return results
