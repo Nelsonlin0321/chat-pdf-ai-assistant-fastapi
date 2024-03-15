0447d86ae15a6f1e9f138d297ccf607e59b90d5c
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-03-15 17:58:31 +0800

    unable timeout

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --workers=${WORKERS:-3} --threads ${THREADS:-3} --timeout 60 --bind :${PORT:-8000} --worker-class uvicorn.workers.UvicornWorker server:app
+gunicorn --workers=${WORKERS:-3} --threads ${THREADS:-3} --timeout 0 --bind :${PORT:-8000} --worker-class uvicorn.workers.UvicornWorker server:app
